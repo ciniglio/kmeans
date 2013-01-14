a897c6207a75b5d7ea5328c7a2fd56eea10f42cd
Alejandro Ciniglio <mail@alejandrociniglio.com>
2013-01-14 00:00:53 -0500

    Made helper functions public.

--- lib/kmeans/util.rb
+++ lib/kmeans/util.rb
@@ -6,28 +6,36 @@ module Kmeans
       @data.to_a.sample n
     end
 
-    def distance(u, v)
-      raise LengthMismatch, "||#{u}|| != ||#{v}||" unless (u.length == v.length)
-      sum = 0.to_f
-      u.each_with_index do |p, i|
-        q = v[i]
-        sum += (p - q)**(2.to_f)
-      end
-      Math.sqrt(sum)
-    end
-
     def new_clusters(n)
       clusters = []
       n.times { clusters << Set.new }
       clusters
     end
 
-    def centroid(points)
-      x = Array.new(points.first.length, 0)
-      points.each do |p|
-        x = x.zip(p).map { |xi| xi.inject(:+) }
-      end
-      x.map { |xi| xi / points.length.to_f }
+    def distance(u,v)
+      Kmeans::distance(u,v)
     end
+
+    def centroid(p)
+      Kmeans::centroid(p)
+    end
+  end
+
+  def self.distance(u, v)
+    raise LengthMismatch, "||#{u}|| != ||#{v}||" unless (u.length == v.length)
+    sum = 0.to_f
+    u.each_with_index do |p, i|
+      q = v[i]
+      sum += (p - q)**(2.to_f)
+    end
+    Math.sqrt(sum)
+  end
+
+  def self.centroid(points)
+    x = Array.new(points.first.length, 0)
+    points.each do |p|
+      x = x.zip(p).map { |xi| xi.inject(:+) }
+    end
+    x.map { |xi| xi / points.length.to_f }
   end
 end
